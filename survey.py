6395faf8b17beec3ddd4493930d753eff8ec1786
scign <scignspam.github@mailnull.com>
2021-05-19 23:46:24 -0400

    Process separately - continue in case of error

--- survey.py
+++ survey.py
@@ -71,27 +71,31 @@ def get_responses(survey_id, download_path):
   b.quit()
   return num_responses, new_file
 
-def remove_duplicates(df, completion_threshold):
+def remove_duplicates(df, completion_threshold = None):
   logging.info(f'Found [rows, columns]: {df.shape}')
 
-  logging.info('Removing duplicates')
-  # ignore first 4 columns: SID, Serial, Completed Time, Submitted Time (all guaranteed unique)
-  df['all_cols'] = df[df.columns[4:]].apply(
-    lambda x: ''.join(x.dropna().astype(str)), axis=1
-  )
-  dfx = []
-  # group by the remaining columns
-  for n,g in df.groupby('all_cols'):
-    dfg = g.sort_values('Submitted Time')
-    dfg['s_diff'] = dfg['Submitted Time'].diff().dt.total_seconds()
-    # filter where responses within the group are closer than the completion_threshold and drop them
-    dfx.append(dfg[~(dfg.s_diff < completion_threshold)].drop(columns=['all_cols','s_diff']))
+  dups_removed = None
+  if completion_threshold:
+    logging.info('Removing duplicates')
+    # ignore first 4 columns: SID, Serial, Completed Time, Submitted Time (all guaranteed unique)
+    df['all_cols'] = df[df.columns[4:]].apply(
+      lambda x: ''.join(x.dropna().astype(str)), axis=1
+    )
+    dfx = []
+    # group by the remaining columns
+    for n,g in df.groupby('all_cols'):
+      dfg = g.sort_values('Submitted Time')
+      dfg['s_diff'] = dfg['Submitted Time'].diff().dt.total_seconds()
+      # filter where responses within the group are closer than the completion_threshold and drop them
+      dfx.append(dfg[~(dfg.s_diff < completion_threshold)].drop(columns=['all_cols','s_diff']))
 
-  # recombine
-  dups_removed = pd.concat(dfx)
-  dups = df.shape[0] - dups_removed.shape[0]
-  dups_pct = dups / df.shape[0]
-  logging.info(f'Final [rows, columns]: {dups_removed.shape} - {dups} ({dups_pct:0.1%}) duplicates removed')
+    # recombine
+    dups_removed = pd.concat(dfx)
+    dups = df.shape[0] - dups_removed.shape[0]
+    dups_pct = dups / df.shape[0]
+    logging.info(f'Final [rows, columns]: {dups_removed.shape} - {dups} ({dups_pct:0.1%}) duplicates removed')
+  else:
+    dups_removed = df.copy()
 
   common_cols = ['Serial','SID','Submitted Time','Completed Time','Draft','IP Address','UID','Username','survey']
   return dups_removed.melt(common_cols, var_name='Question', value_name='Response')
@@ -99,25 +103,33 @@ def remove_duplicates(df, completion_threshold):
 
 @display.virtual_display
 def download_surveys():
-  with TemporaryDirectory() as download_path:
-    for survey_id in surveys.keys():
-      survey_file = surveys[survey_id][0]
-      survey_name = surveys[survey_id][1]
-      completion_threshold = surveys[survey_id][2]
-      num_responses, new_file = get_responses(survey_id, download_path)
+  for survey_id in surveys.keys():
+    with TemporaryDirectory() as download_path:
+      try:
+        survey_file = surveys[survey_id][0]
+        survey_name = surveys[survey_id][1]
+        completion_threshold = surveys[survey_id][2]
+        
+        # open browser, download responses - takes time
+        num_responses, new_file = get_responses(survey_id, download_path)
 
-      if num_responses and new_file:
-        logging.info('Opening file')
-        df = pd.read_excel(new_file, skiprows=2, sheet_name="Sheet1", engine='openpyxl')
-        df['survey'] = survey_name
+        # if successful, num_responses > 0 and new_file <> ''
+        if num_responses and new_file:
+          logging.info('Opening file')
+          df = pd.read_excel(new_file, skiprows=2, sheet_name="Sheet1", engine='openpyxl')
+          df['survey'] = survey_name
 
-        output_file = os.path.join(download_path, survey_file)
-        melted = remove_duplicates(df, completion_threshold)
-        melted.to_excel(output_file)
+          output_file = os.path.join(download_path, survey_file)
+          melted = remove_duplicates(df, completion_threshold)
+          melted.to_excel(output_file)
 
-        upload_to_sharepoint(output_file, survey_file)
+          # use the office365 api to upload the file
+          upload_to_sharepoint(output_file, survey_file)
 
-        logging.info(f'------ Completed ({survey_id} - {survey_name}) ------')
+          logging.info(f'------ Completed ({survey_id} - {survey_name}) ------')
+      
+      except:
+        logging.exception(f'Survey {survey_id} failed.')
 
 """
   Set up survey datasets to download
@@ -134,6 +146,14 @@ surveys = {
   '45402': ('202105_pp_g3_family.xlsx', 'Pre-Primary - Grade 3 family 2021 year end', 60),
   '45401': ('202105_g7_g12_student.xlsx', 'Secondary student 2021 year end', 60),
   '45403': ('202105_g7_g12_parent.xlsx', 'Secondary parent 2021 year end', 60),
+  '46559': ('202105_lt.xlsx', 'Leadership team 2021 year end', 60),
+  # mid-year surveys?
+  #'43121': ('202105_pp_p_teacher_mid.xlsx', 'Pre-Primary and Primary teachers 2021 mid year', 60),
+  #'42518': ('202105_g4_g6_student_mid.xlsx', 'Grade 4-6 student 2021 mid year', 60),
+  #'': ('202105_g4_g6_parent_mid.xlsx', 'Grade 4-6 parent 2021 mid year', 60),
+  #'': ('202105_pp_g3_family.xlsx_mid', 'Pre-Primary - Grade 3 family 2021 mid year', 60),
+  #'42509': ('202105_g7_g12_student.xlsx_mid', 'Secondary student 2021 mid year', 60),
+  #'': ('202105_g7_g12_parent.xlsx_mid', 'Secondary parent 2021 mid year', 60),
 }
 
 if __name__ == '__main__':
